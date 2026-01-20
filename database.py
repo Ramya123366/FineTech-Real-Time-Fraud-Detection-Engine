90505e149d0466c93fe99f60e710274386dac741
Ramya123366 <rakkinapelli@gmail.com>
2026-01-20 14:24:15 +0530

    Add files via upload

---
+++ database.py
@@ -0,0 +1,16 @@
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
+from sqlalchemy.orm import sessionmaker, declarative_base
+
+DATABASE_URL = "postgresql+asyncpg://postgres:password@localhost:5432/frauddb"
+
+engine = create_async_engine(DATABASE_URL, echo=True)
+
+AsyncSessionLocal = sessionmaker(
+    engine, class_=AsyncSession, expire_on_commit=False
+)
+
+Base = declarative_base()
+
+async def get_db():
+    async with AsyncSessionLocal() as session:
+        yield session
